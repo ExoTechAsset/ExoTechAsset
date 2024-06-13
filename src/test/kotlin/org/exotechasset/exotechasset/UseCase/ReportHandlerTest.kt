5041376acb9dbbddc5111f5860b7e00f02f5b140
NTUT_chris85618 <t112598078@ntut.edu.tw>
2024-06-13 10:19:10 +0800

    Update ReportHandlerTest.kt

--- src/test/kotlin/org/exotechasset/exotechasset/UseCase/ReportHandlerTest.kt
+++ src/test/kotlin/org/exotechasset/exotechasset/UseCase/ReportHandlerTest.kt
@@ -77,25 +77,4 @@ class ReportHandlerTest {
 
         kotlin.test.assertEquals(expects, result.toString());
     }
-
-    // @Test
-    // fun generateCsvTest(){
-    //     val assetHandler = AssetHandler()
-    //     var mutablemap  = mutableMapOf<AssetGetBy, Any>(AssetGetBy.ID to 1, AssetGetBy.STATUS to 2)
-    //     var metric: Metric = Metric(mutablemap)
-    //     var result:Any
-    //     var expects:Any
-    //     val asset1 = ConcreteAsset("Asset 1")
-    //     val asset2 = ConcreteAsset("Asset 2")
-
-    //     assetHandler.addNewAsset(asset1)
-    //     assetHandler.addNewAsset(asset2)
-    //     val report = ReportHandler(assetHandler).generateReport(ReportType.CSV, metric)
-    //     result = report.get()
-    //     expects = "id, status, assignee, auditDate, location, changelog, parentId\n" +
-    //             "Asset 1, Deployable, null, null, , [], \n" +
-    //             "Asset 2, Deployable, null, null, , [], \n"
-
-    //     kotlin.test.assertEquals(expects, result.toString());
-    // }
 }
